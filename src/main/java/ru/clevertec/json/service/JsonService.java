5d46c32c96b4cee6dc670d84b17d5025224ea181
Stanislau Kachan <Lancer397@gmail.com>
2023-03-02 21:34:49 +0300

    refactor: renaming a method in JsonPattern.java

--- src/main/java/ru/clevertec/json/service/JsonService.java
+++ src/main/java/ru/clevertec/json/service/JsonService.java
@@ -139,7 +139,7 @@ public class JsonService {
 
     public List<String> findObjectsInJsonArray(Field[] fields, String json) {
         List<String> objects = new ArrayList<>();
-        Pattern splitPattern = JsonPattern.jsonArraySplitterPattern(fields);
+        Pattern splitPattern = JsonPattern.jsonArrayOfCustomObjectsSplitterPattern(fields);
         Matcher objectsMatcher = splitPattern.matcher(json);
         while (objectsMatcher.find()) {
             String matchGroup = objectsMatcher.group();
