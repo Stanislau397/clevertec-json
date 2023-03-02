cdccc7e7d07970477dc5aeb1a3fa05b444bf8357
Stanislau Kachan <Lancer397@gmail.com>
2023-03-02 19:02:57 +0300

    add: adding tests for arrayService

---
+++ src/test/java/ru/clevertec/json/data/TestDataForArrayService.java
@@ -0,0 +1,13 @@
+package ru.clevertec.json.data;
+
+public class TestDataForArrayService {
+
+    private Genre[] genres;
+    private boolean[] booleans;
+    private int[] ints;
+    private byte[] bytes;
+    private short[] shorts;
+    private long[] longs;
+    private float[] floats;
+    private double[] doubles;
+}
